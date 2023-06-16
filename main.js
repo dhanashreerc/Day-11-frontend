43292ae5578a05b00bba2b19064c6fd36743e606
dhanashree <dhanichavan795@gmail.com>
2023-06-16 18:29:26 +0530

    frontend-integration

--- main.js
+++ main.js
@@ -7,5 +7,21 @@ function addRecord(){
     //connect the backend-ajax
     fetch(uri);
 
-    message.value="";
+    inputMessageRef.value ="";
+
+    readRecord();
+}
+
+async function readRecord(){
+    const uri = "http://localhost:3000/findAll";
+
+    let res = await fetch(uri);
+    let list = await res.json();
+
+    for(let item of list){
+        let parentRef = document.querySelector("#parent");
+        let existingVal = parentRef.innerHTML;
+        let newVal = `<h1>${item.message}</h1>`;
+        parentRef.innerHTML = newVal + existingVal;
+    }
 }
